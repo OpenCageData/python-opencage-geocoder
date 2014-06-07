a17dae3f47fa0cfc738d6834f3000f8761864122
Rory McCann <rory@technomancy.org>
2014-06-07 09:11:59 +0100

    here we go
    replace tabs with spaces

--- opencage/geocoder.py
+++ opencage/geocoder.py
@@ -1,19 +1,19 @@
-import requests, json
+import requests
 
 class OpenCageGeocode:
-	url = 'http://prototype.opencagedata.com/geocode/v1/json'
-	key = ''
+    url = 'http://prototype.opencagedata.com/geocode/v1/json'
+    key = ''
 
-	def __init__(self, key):
-		self.key = key
+    def __init__(self, key):
+        self.key = key
 
-	def geocode(self, query):
-		return self.getJSON(query)
+    def geocode(self, query):
+        return self.getJSON(query)
 
-	def getJSON(self, query):
-		data = {
-			'q': query,
-			'key': self.key
-		}
-		url = self.url
-		return requests.get(url, params=data)
+    def getJSON(self, query):
+        data = {
+            'q': query,
+            'key': self.key
+        }
+        url = self.url
+        return requests.get(url, params=data)
